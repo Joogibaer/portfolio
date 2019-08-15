2704cc3e2e9a637f4b3e3caca93fff546997d486
Felix Baertschi <baertschi91@gmail.com>
2019-08-15 09:39:18 +0200

    add edu page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './about/education/education.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-    { path: 'home', component: HomeComponent },
-    { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'about/education', component: EducationComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './about/education/education.component';
 
 
 @NgModule({
@@ -19,13 +22,15 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    EducationComponent,
   ],
   imports: [
     TooltipModule.forRoot(),//ngx-bootstrap
     BsDropdownModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
-    TabsModule.forRoot(),
+    TabsModule.forRoot(),//ngx-bootstrap
+    BrowserAnimationsModule, //Materials
   ],
   providers: [],
   bootstrap: [AppComponent]
