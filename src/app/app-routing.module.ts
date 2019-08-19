6208c4dd97047e7985e53a4262b30eb9dfcea2d4
Felix Baertschi <baertschi91@gmail.com>
2019-08-19 10:32:21 +0200

    add redirect 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   { path: 'about/skills', component: SkillsComponent },
   { path: '404', component: HomeComponent },
   { path: 'baertschi', component: HomeComponent },
+  {path: '**', redirectTo: '/404'}
 
 
 ];
